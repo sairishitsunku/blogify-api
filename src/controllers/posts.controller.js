a8a80d9b58beead014d34aaf3325d3829dc8e8dc
Rishit.S <sairishitsunku@gmail.com>
2026-01-27 09:13:45 +0530

    Added dynamic routes

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -2,4 +2,9 @@ const getAllPosts = (req, res) => {
     res.status(200).json({ message: 'Route handled by postController.getAllPosts' });
 };
 
-module.exports = { getAllPosts };
+const getPostById = (req, res) => {
+    const postId = req.params.postId;
+    res.status(200).json({ message: `Requested post with ID: ${postId}` });
+};
+
+module.exports = { getAllPosts, getPostById };
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -4,5 +4,6 @@ const postController = require('../controllers/posts.controller');
 const router = express.Router();
 
 router.get('/', postController.getAllPosts);
+router.get('/:postId', postController.getPostById);
 
 module.exports = router;
